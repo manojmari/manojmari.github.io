cb878bf2a289f72dab220e705b04c3c0f74d25b2
crosssticker <pradeep.mariappan@gmail.com>
2015-11-07 19:19:19 -0500

    goddammit

--- engagement/script/script.js
+++ engagement/script/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	loadXmlAndContinue();
+	$('#inputName').focus();
 });
 
 function loadXmlAndContinue(){
@@ -64,6 +65,25 @@ console.log(friends)
 	return false;
 }
 
+function loadFutureImages(friend){
+	var index = 0;
+	_.each(friend.images, function(currImg){
+		var imgDom = document.createElement('img');
+		imgDom.src = 'data/images/' + currImg.file;
+		imgDom.id = 'img_' + index;
+		imgDom.className = 'imgShow_imgs';
+		if(index ==0)
+			imgDom.style.display = 'inline';
+		$('#imageShow').append(imgDom);
+		index++;
+	});
+	$('#imageShow').data({'current' : 0 ,'maxIndex' : index });
+	$('#leftNav').click(showLeft);
+	$('#rightNav').click(showRight);
+
+	//var maxIndex = $('#imageShow').data();
+}
+
 function loadDragDrop(name){
 	var friends = $(document).data('friends'),
 		groups = $(document).data('groups'),
@@ -71,11 +91,96 @@ function loadDragDrop(name){
 			return curr.name.toLowerCase() == name.toLowerCase()}),
 		group = _.find(groups, {id: friend.group});
 
-	console.log(groups)
-	console.log(friend)
-	console.log(group)
+	loadFutureImages(friend);
+	$('#user-info').hide();
+	$('#dragDrop').show();
+	var puzzleHtml = "";
+	var referenceHtml = "";
 
+	for(var i = 0; i<3 ; i++){
+		for(var j = 0; j<3 ; j++){
+			var backPosX = j * 150;
+			var backPosY = i * 150;
+			puzzleHtml += "<div class='puzzlePiece tiles positionRelative' style='"
+			+" background-position:-" + backPosX+ "px -" + backPosY+ "px;"
+			+"-webkit-mask-image: url(data/svg/" + i + j + ".svg);' dragId="+i+j+"></div>";
+			referenceHtml += "<img class='tiles droppables'  dropId="+i+j+" style='top:" + (i * 150) + "px;left:" + (j * 150) + "px' src='data/svg/" + i + j + ".svg' />"
+		}	
+	}
+	console.log(puzzleHtml)
+	$('#draggableContainer').html(puzzleHtml);
+	$('#referenceTiles').html(referenceHtml);
+	var draggablePieces = $('.puzzlePiece').css("background-image", "url('data/images/" + group.file + "')").draggable({
+		revert: 'invalid',
+		drag: function() {
+	        $(this).css('z-index', 10);
+	    },
+	    stop: function() {
+	        $(this).css('z-index', 1);
+	    }
+	});
+	shuffle(draggablePieces);
+	_.each(draggablePieces, function(currentDraggable, index){
+			$('#draggableContainer').append(currentDraggable);
+	})
+	$('.droppables').droppable({
+		accept: function(event){
+			return $(event).attr('dragId') === $(this).attr('dropId')
+		},
+		drop: function(event, ui){
+			var dropId = $(this).attr('dropId');
+			var y = parseInt(dropId.charAt(0))
+			var x = parseInt(dropId.charAt(1))
+			$('#draggableResultContainer').append(ui.draggable);
+			$(ui.draggable).css({top: y*150+'px', left: x*150+'px'}).draggable({'disabled': true}).removeClass('positionRelative');
+			if($('#draggableResultContainer').children().length == 10){
+				$('#dragResultText').html("So " + name + " you DO remember this picture!! Come, let's checkout some more...");
+				$('#draggableContainer').slideUp(function(){
+					$('#dragDropResult').slideDown();
+					$('#imageShow').fadeIn();
+				});
+				$(document).bind('keydown',function(e){
+					if(e.keyCode == 37)
+						showLeft();
+					if(e.keyCode == 39)
+						showRight();
+				})
+			}
+		}
+	});
+	$('#draggableContainer').hide().slideDown()
 	return false;
+}
+
+function showLeft(){
+	var prevIndex = $('#imageShow').data('current');
+	var maxIndex = $('#imageShow').data('maxIndex');
+	console.log(prevIndex)
+
+	var currentIndex = prevIndex - 1
+	if(currentIndex == -1)
+		currentIndex = maxIndex - 1;
+	console.log(currentIndex)
+	console.log(maxIndex)
+	$('#img_' + prevIndex).hide("slide", { direction: "right" }, 300, function(){
+	});
+		$('#img_' + currentIndex).show("slide", { direction: "left" }, 300);
+	$('#imageShow').data({'current': currentIndex});
+}
+
+function showRight(){
+	var prevIndex = $('#imageShow').data('current');
+	var maxIndex = $('#imageShow').data('maxIndex');
+
+	var currentIndex = prevIndex + 1
+	if(currentIndex == maxIndex)
+		currentIndex = 0;
+	console.log(currentIndex)
+	console.log(maxIndex)
+	$('#img_' + prevIndex).hide("slide", { direction: "left" }, 300, function(){
+	});
+		$('#img_' + currentIndex).show("slide", { direction: "right" }, 300);
+	$('#imageShow').data({'current': currentIndex});
 }
 
 function renderAttributes(oThis){
@@ -106,4 +211,23 @@ function createFriends(friends){
   		arrFriends.push(currentFriend);
 	})
 	$(document).data('friends', arrFriends);
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
